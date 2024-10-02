eb6975ee0a4c752d4400085a10300fc85c70e496
yolvinruiz <yolvitoruiz@gmail.com>
2024-10-02 15:05:37 -0500

    cambios 20

--- Capa_entidad/entEmpleado.cs
+++ Capa_entidad/entEmpleado.cs
@@ -14,6 +14,8 @@ namespace Capa_entidad
         public int DNI { get; set; }
         public int Telefono { get; set; }
         public string Correo { get; set; }
+        public string NombreCargo { get; set; }
+        public bool Estado { get; set; }
         public int id_Cargo { get; set; }
         public bool Estado { get; set; }
         public string NombreCargo { get; set; }
